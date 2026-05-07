42fc49a8b03fc6f569b5eb01ce93ed69103500e6
Gina <gn.faida87@gmail.com>
2026-05-07 23:20:17 +0700

    d1-config

--- lib/core/network/d1_config.dart
+++ lib/core/network/d1_config.dart
@@ -1,6 +1,6 @@
 class D1Config {
   // Ganti URL ini dengan URL Worker yang Anda dapatkan setelah mendeploy Worker di Cloudflare
-  static const String baseUrl = "https://sistem-informasi-ma-api.gnfaida87.workers.dev";
+  static const String baseUrl = "https://sistem-informasi-ma-api.gn-faida87.workers.dev";
   
   // Jika Anda menambahkan API Key di Worker untuk keamanan
   static const String apiKey = "";
